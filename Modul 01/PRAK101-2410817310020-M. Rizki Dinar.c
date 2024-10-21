a6866553cf1cdade0c9a366d8758a9792410a2d2
JohnathanRH <144834953+JohnathanRH@users.noreply.github.com>
2024-10-22 00:19:03 +0800

    Soal 1 Up test

---
+++ Modul 01/PRAK101-2410817310020-M. Rizki Dinar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y,z, sum;
+    x = 5;
+    y = 7;
+    z = 9;
+    sum = x+y+z;
+
+    printf("Variabel x bernilai %i\n", x);
+    printf("Variabel y bernilai %i\n", y);
+    printf("Variabel z bernilai %i\n", z);
+    printf("Jumlah variabel tersebut adalah %i", sum);
+    
+    return 0;
+}
---
+++ Modul 01/PRAK101-2410817310020-M. Rizki Dinar.py
@@ -0,0 +1,9 @@
+x = 5
+y = 7
+z = 9
+Sum = x+y+z
+
+print("Variabel x bernilai " + str(x))
+print("Variabel y bernilai " + str(y))
+print("Variabel z bernilai " + str(z))
+print("Jumlah variabel tersebut adalah " + str(Sum))
